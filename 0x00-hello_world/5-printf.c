b997ddae0eb2ec3a73b06fa40ff7a095a7c4fa6c
Idongesit <jtony7228@gmail.com>
2023-08-12 13:59:03 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is a string 
+ * Return: this end it all
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0)
+}
