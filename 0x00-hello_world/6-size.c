b941e7352ecdc2a27aa93a5670f51a3cd4bd98d9
Idongesit <jtony7228@gmail.com>
2023-08-12 19:02:44 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - checking the size of my computer
+ *
+ * Return: makes it successful
+ */
+
+int main(void)
+{
+	char b;
+	int c;
+	long int d;
+	long long int e;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+	return (0);
+}
