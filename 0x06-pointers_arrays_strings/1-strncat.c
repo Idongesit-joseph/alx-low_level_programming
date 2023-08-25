b136ceac4b16da3e6296a9b8eb87edda5d982f79
Idongesit <jtony7228@gmail.com>
2023-08-25 15:50:01 -0700

    git

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncat - concatenate two strings
@@ -10,12 +11,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len = _strnlen(dest);
+	int len = strlen(dest);
 	int i;
 
 	for (i = 0 ; i < n && *src != '\0' ; i++)
 	{
-		dest[len + i] = src[i];
+		dest[len + i] = *src;
 		src++;
 	}
 	dest[len + i] = '\0';
