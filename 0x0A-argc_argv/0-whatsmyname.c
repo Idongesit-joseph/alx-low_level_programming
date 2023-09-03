26ea0a887dcbfe071911815906c73e1d181bf9da
Idongesit <jtony7228@gmail.com>
2023-09-02 23:08:34 -0700

    git

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - star the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Reurn: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
