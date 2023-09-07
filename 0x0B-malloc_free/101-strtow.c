90fbbc646f3727a9040b78044de04a52821f9390
Idongesit <jtony7228@gmail.com>
2023-09-06 22:30:37 -0700

    git

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 
-int word_len(char */str);
+int word_len(char *str);
 int count_words(char *str);
-char ** strtow(char *str);
+char **strtow(char *str);
 
 /**
  * word_len - kgkg
@@ -30,19 +30,20 @@ int word_len(char *str)
  */
 
 int count_words(char *str)
+{
 	int index = 0, words = 0, len = 0;
 
 	for (index = 0; *(str + index); index++)
 		len++;
 	for (index = 0; index < len; index++)
 	{
-		if (*(str + index) !=' ')
+		if (*(str + index) != ' ')
 		{
 			words++;
 			index += word_len(str + index);
 		}
 	}
-	return (words)
+	return (words);
 }
 
 /**
@@ -54,7 +55,7 @@ int count_words(char *str)
 char **strtow(char *str)
 {
 	char **strings;
-	injt index = 0, words, w, letters, l;
+	int index = 0, words, w, letters, l;
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
@@ -65,4 +66,21 @@ char **strtow(char *str)
 	if (strings == NULL)
 		return (NULL);
 	for (w = 0; w < words; w++)
-	
+	{
+		while (str[index] == ' ')
+			index++;
+		letters = word_len(str + index);
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+		if (strings[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(strings[w]);
+			free(strings);
+			return (NULL);
+		}
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
+	}
+	strings[w] = NULL;
+	return (strings);
+}
