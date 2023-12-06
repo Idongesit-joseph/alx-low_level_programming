e67cdf78339b17ab56b00ddfd07c3be2145aca48
Idongesit <jtony7228@gmail.com>
2023-12-05 18:37:47 -0800

    hmm

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - www
+ * @size: ssss
+ * Return: jjj
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	if (size == 0)
+		return (NULL);
+
+	table = calloc(1, sizeof(hash_table_t));
+	if (size == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t));
+	if (table == NULL)
+	{
+		free(table);
+		return(NULL);
+	}
+	return (table);
+}
